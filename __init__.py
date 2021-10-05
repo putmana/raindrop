98e9ea77bcd8a4b0f3c98a4223b4e8b724a2af44
adam <adam.putman@outlook.com>
2021-10-05 13:27:09 -0600

    Designed a mock-up of the search page

--- __init__.py
+++ __init__.py
@@ -2,8 +2,12 @@ from flask import Flask, render_template
 app = Flask(__name__)
 
 @app.route('/')
-def hello_world():
-    return render_template('hello.html', title="Word")
+def search():
+    return render_template('search.html', title="Word")
+
+@app.route('/results')
+def results():
+    return render_template('results.html', title="Word")
 
 if __name__ == "__main__":
     app.run()
