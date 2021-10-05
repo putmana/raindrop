9c6e27a6c0037d26e6965393ba6bbe0e7f6c6db8
adam <adam.putman@outlook.com>
2021-10-05 09:43:20 -0600

    Removed API components and spun them off into separate GitHub project

--- __init__.py
+++ __init__.py
@@ -1,14 +1,9 @@
 from flask import Flask, render_template
-import api_fetch as api
 app = Flask(__name__)
 
 @app.route('/')
 def hello_world():
     return render_template('hello.html', title="Word")
-
-@app.route('/api', methods=['GET'])
-def api_fetch():
-    return api.json_test()
 
 if __name__ == "__main__":
     app.run()
